7083abf848caf0a08e1d7122c173e051f494eb14
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-03-27 17:43:53 -0400

    Added code coverage for headers

---
+++ tests/HeadersTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patoui\Router\Tests;
+
+use Patoui\Router\Headers;
+
+class HeadersTest extends TestCase
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->backupGlobals = true;
+    }
+
+    public function test_get_headers_from_globals(): void
+    {
+        // Arrange
+        $_SERVER['HTTP_ACCEPT'] = 'application/json';
+
+        // Act
+        $headers = Headers::getHeadersArrayFromGlobals();
+
+        // Assert
+        self::assertCount(1, $headers);
+        self::assertEquals('application/json', $headers['ACCEPT'][0]);
+    }
+}
