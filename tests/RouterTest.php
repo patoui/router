127863f3bffdf1e0bada7a349de41aeb8040aadc
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-03-27 20:53:14 -0400

    Added test coverage for router

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Patoui\Router\Tests;
 
 use Patoui\Router\Route;
+use Patoui\Router\RouteNotFoundException;
 use Patoui\Router\Router;
 use Patoui\Router\Uri;
 
@@ -27,8 +28,8 @@ class RouterTest extends TestCase
         $routes = $router->getRoutes();
 
         // Assert
-        $this->assertEquals(1, count($routes));
-        $this->assertEquals($route, $routes[0]);
+        self::assertEquals(1, count($routes));
+        self::assertEquals($route, $routes[0]);
     }
 
     public function test_can_add_route(): void
@@ -48,8 +49,8 @@ class RouterTest extends TestCase
 
         // Assert
         $routes = $router->getRoutes();
-        $this->assertEquals(1, count($routes));
-        $this->assertEquals($route, $routes[0]);
+        self::assertEquals(1, count($routes));
+        self::assertEquals($route, $routes[0]);
     }
 
     public function test_can_resolve_route(): void
@@ -79,9 +80,9 @@ class RouterTest extends TestCase
 
         // Assert
         $routes = $router->getRoutes();
-        $this->assertEquals(2, count($routes));
-        $this->assertEquals(get_class($aboutController), $resolvedRoute->getClassName());
-        $this->assertEquals('index', $resolvedRoute->getClassMethodName());
+        self::assertEquals(2, count($routes));
+        self::assertEquals(get_class($aboutController), $resolvedRoute->getClassName());
+        self::assertEquals('index', $resolvedRoute->getClassMethodName());
     }
 
     public function test_can_resolve_route_with_parameters(): void
@@ -103,10 +104,10 @@ class RouterTest extends TestCase
 
         // Assert
         $routes = $router->getRoutes();
-        $this->assertCount(1, $routes);
-        $this->assertEquals(get_class($postController), $resolvedRoute->getClassName());
-        $this->assertEquals('show', $resolvedRoute->getClassMethodName());
-        $this->assertEquals(['id' => '123'], $resolvedRoute->getParameters());
+        self::assertCount(1, $routes);
+        self::assertEquals(get_class($postController), $resolvedRoute->getClassName());
+        self::assertEquals('show', $resolvedRoute->getClassMethodName());
+        self::assertEquals(['id' => '123'], $resolvedRoute->getParameters());
     }
 
     public function test_can_resolve_route_with_casted_parameters(): void
@@ -128,9 +129,20 @@ class RouterTest extends TestCase
 
         // Assert
         $routes = $router->getRoutes();
-        $this->assertCount(1, $routes);
-        $this->assertEquals(get_class($postController), $resolvedRoute->getClassName());
-        $this->assertEquals('show', $resolvedRoute->getClassMethodName());
-        $this->assertSame(123, $resolvedRoute->getParameters()['id']);
+        self::assertCount(1, $routes);
+        self::assertEquals(get_class($postController), $resolvedRoute->getClassName());
+        self::assertEquals('show', $resolvedRoute->getClassMethodName());
+        self::assertSame(123, $resolvedRoute->getParameters()['id']);
+    }
+
+    public function test_resolve_route_not_found(): void
+    {
+        // Arrange
+        $router = new Router();
+        $serverRequest = $this->getStubServerRequest(['request_target' => '/post/123', new Uri('/post/123')]);
+        $this->expectException(RouteNotFoundException::class);
+
+        // Act && Assert
+        $router->resolve($serverRequest);
     }
 }
